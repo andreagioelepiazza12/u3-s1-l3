34248b39e2a16d3554a7c858b54d93d42bf1c243
andreagioelepiazza12 <andreagioelepiazza@gmail.com>
2024-11-27 16:43:23 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,8 @@ function App() {
     <>
       <MyNav />
       <div className="container mt-4">
-        <h1>Benvenuto nel mio sito!</h1>
-        <p>Questa è una semplice navbar creata con React-Bootstrap.</p>
+        
+        
       </div>
     </>
   );
